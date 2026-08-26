ebbff937cf8846b2da1a911979422dc74ba98b38
TahaaZare <tahaazre.dev@gmail.com>
2026-08-26 15:49:59 +0330

    auth [ login & register

---
+++ app/Http/Controllers/Api/AuthApiController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\LoginApiRequest;
+use App\Http\Requests\Api\RegisterApiRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthApiController extends Controller
+{
+    public function register(RegisterApiRequest $request)
+    {
+        $validated = $request->validated();
+
+        $validated['password'] = Hash::make($validated['password']);
+
+        $newUser = User::create($validated);
+
+        //create token
+        $token = $newUser->createToken("{$newUser->email}-register-token")->plainTextToken;
+
+        return response()->json([
+            'message' => 'User registered successfully',
+            'token' => $token,
+        ], 200);
+    }
+
+    public function login(LoginApiRequest $request)
+    {
+        $validated = $request->validated();
+
+        $user = User::where('email', $validated['email'])->first();
+
+        if(!Hash::check($validated['password'], $user->password)){
+            return response()->json([
+                'message' => 'Wrong password',
+            ],403);
+        }
+
+        //delete tokens
+//        $user->tokens()->delete();
+        $user->tokens()->where('name',"{$user->email}-register-token")->delete();
+
+        //create token
+        $token = $user->createToken("{$user->email}-login-token")->plainTextToken;
+        return response()->json([
+            'message' => 'User login successfully',
+            'token' => $token,
+        ], 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::controller(\App\Http\Controllers\Api\AuthApiController::class)
+    ->prefix('auth')
+    ->group(function () {
+       Route::post('register','register');
+
+       Route::post('login','login');
+    });
